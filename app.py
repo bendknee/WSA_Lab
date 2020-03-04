981013383f4132094d262c54fd69d7d68631a716
Benny William Pardede <bpardedewilliam@gmail.com>
2020-03-04 08:39:25 +0700

    Master class

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 from flask import Flask, render_template, request, abort, Response
 
-from api import numbers_api
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('')
 
 
 @app.route('/api/<path:sub_api>', methods=['GET', 'POST'])
 def api(sub_api):
-    if sub_api == 'numbers':
-        try:
-            return render_template('index.html', fact=numbers_api(request.args))
-        except IOError:
-            abort(Response("Bad Request Exception: Missing number parameter", status=400))
+    pass
 
 
 if __name__ == '__main__':
