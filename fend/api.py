fec16ddf3455203dc457a27d7184109a671cd7a5
Benny William Pardede <bpardedewilliam@gmail.com>
2020-03-22 19:51:12 +0700

    Fend -> Bend take 6

--- fend/api.py
+++ fend/api.py
@@ -7,7 +7,7 @@ FILE_ARG = "file"
 
 
 def relay_file_to_backend(files):
-    url = "{:s}:{:s}/{:s}".format(BEND_URL, BEND_PORT, BEND_URI)
+    url = "http://{:s}:{:s}/{:s}".format(BEND_URL, BEND_PORT, BEND_URI)
     all_files = [(FILE_ARG, (f.filename, f.stream, f.mimetype)) for f in files]
     req = r.post(url, files=all_files)
     return req
