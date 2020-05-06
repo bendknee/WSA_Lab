ffc93eac65baf6b930236d68373d8b5ff5b31357
Benny William Pardede <bpardedewilliam@gmail.com>
2020-05-06 17:26:21 +0700

    COTS 3 Take 1

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
-from flask import Flask, render_template, request, abort, Response
+import multiprocessing
+import uuid
+
+from flask import Flask, render_template, request, abort, Response, redirect, url_for, send_from_directory
+
+import daemon
+
+URL_ARG = "link"
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = daemon.DIR_PATH
 
 
 @app.route('/')
 def index():
-    return render_template('')
+    return render_template('index.html')
 
 
-@app.route('/api/<path:sub_api>', methods=['GET', 'POST'])
-def api(sub_api):
-    pass
+@app.route('/progress/<uuid:route>', methods=['GET'])
+def progress(route):
+    return render_template('progress.html', route=route)
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form.get(URL_ARG)
+    if url is None:
+        abort(Response("`{:s}` argument required".format(URL_ARG), status=400))
+    if len(url) == 0:
+        abort(Response("`{:s}` argument is empty".format(URL_ARG), status=400))
+
+    routing_key = str(uuid.uuid4())
+    execute_daemon(url, routing_key)
+
+    return redirect(url_for("progress", route=routing_key))
+
+
+@app.route('/retrieve/<filename>', methods=['GET'])
+def retrieve(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+def execute_daemon(url, routing_key):
+    pool = multiprocessing.Pool()
+    pool.apply_async(daemon.execute, (url, routing_key,))
+    pool.close()
 
 
 if __name__ == '__main__':
---
+++ daemon.py
@@ -0,0 +1,57 @@
+import time
+
+import pika
+import requests
+from flask import url_for
+
+CREDENTIAL_KEY = "0806444524"
+MY_NPM = "1606917550"
+RABBIT_HOST = "152.118.148.95"
+RABBIT_PORT = 5672
+
+PROGRESS_TEMPLATE = "Downloading {:s}: {:.2f}%"
+
+DIR_PATH = '/home/cots/'
+
+
+def execute(url, routing_key):
+    send_message("Initiate download {:s}".format(url), routing_key)
+    download_file(url, routing_key)
+
+
+def download_file(url, routing_key):
+    filename = url.split('/')[-1]
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    file = open(DIR_PATH + filename, 'wb')
+    size = response.headers.get('content-length')
+
+    if size is None:  # no content length header
+        file.write(response.content)
+    else:
+        written = 0
+        for chunk in response.iter_content(chunk_size=512):
+            written += len(chunk)
+            progress = (written / int(size)) * 100
+            send_message(PROGRESS_TEMPLATE.format(url, progress), routing_key)
+            file.write(chunk)
+
+    response.close()
+    file.close()
+    send_message("Download finished: %s".format(url_for("retrieve", filename=filename)), routing_key)
+
+
+def send_message(message, route):
+    time.sleep(0.6)
+    cred = pika.PlainCredentials(username=CREDENTIAL_KEY, password=CREDENTIAL_KEY)
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBIT_HOST, port=RABBIT_PORT,
+                                  virtual_host="/" + CREDENTIAL_KEY, credentials=cred)
+    )
+    channel = connection.channel()
+    channel.exchange_declare(exchange=MY_NPM, exchange_type='direct')
+    channel.basic_publish(exchange=MY_NPM, routing_key=route, body=message)
+    connection.close()
+    time.sleep(0.6)
